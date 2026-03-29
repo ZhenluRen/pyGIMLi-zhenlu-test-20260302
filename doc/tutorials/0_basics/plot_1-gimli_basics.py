8c9a190582a36c68cd72cf4e196432e0f67a699f
ZhenluRen <80757915+ZhenluRen@users.noreply.github.com>
2026-03-29 16:43:12 +0200

    Fix tutorial text for pygimli aliasing
    Corrected the text in the tutorial regarding aliasing the pygimli library.

--- doc/tutorials/0_basics/plot_1-gimli_basics.py
+++ doc/tutorials/0_basics/plot_1-gimli_basics.py
@@ -15,7 +15,7 @@ This tutorial shows some basic aspects of handling a mesh.
 
 First, the library needs to be imported.
 To avoid name clashes with other libraries we suggest to ``import pygimli`` and
-alias it to the simple abbreviation ``pg``: CR
+alias it to the simple abbreviation ``pg``: 
 """
 import pygimli as pg
 
